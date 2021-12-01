98b1d243925085d92cdb5e4c1ff4ca6e049957d8
DekiDex <89651615+DekiDex@users.noreply.github.com>
2021-12-01 18:49:00 +0100

    faq

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	$(".btnone").click(function(){
+		$(".pone").toggle();
+		$(".hone").toggle();
+		$(".imageone").toggle();
+	});
+});
+
+$(document).ready(function(){
+	$(".btntwo").click(function(){
+		$(".ptwo").toggle();
+		$(".htwo").toggle();
+		$(".imagetwo").toggle();
+	});
+});
+
+$(document).ready(function(){
+	$(".btnthree").click(function(){
+		$(".pthree").toggle();
+		$(".hthree").toggle();
+		$(".imagethree").toggle();
+	});
+});
+
+$(document).ready(function(){
+	$(".btnfour").click(function(){
+		$(".pfour").toggle();
+		$(".hfour").toggle();
+		$(".imagefour").toggle();
+	});
+});
+
+$(document).ready(function(){
+	$(".btnfive").click(function(){
+		$(".pfive").toggle();
+		$(".hfive").toggle();
+		$(".imagefive").toggle();
+    });
+});
+
+
+
+
+
+
+
